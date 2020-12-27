d2dc3a99c7052b637100fa1ee172b98310f5f450
Name idlogush <ivan.dlogush-ext@jci.com>
2020-12-27 19:27:30 +0200

    HW 3-2 Task 3: Filled square without prints from def

---
+++ HomeWork_3-2/Task-3_withData.py
@@ -0,0 +1,16 @@
+def fill_square(size, symbol='*', is_filled=True):
+    res = []
+    for i in range(size):
+        res.append([symbol] * size)
+    if not is_filled:
+        for i in range(1, size - 1):
+            for j in range(1, size - 1):
+                res[i][j] = ' '
+    return res
+
+
+for el in fill_square(5, '#', True):
+    print(*el)
+print()
+for el in fill_square(5, '#', False):
+    print(*el)
